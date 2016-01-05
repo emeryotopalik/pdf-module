862dc55c5b9e45d2d2cc3b042c75dea559439e2a
emeryotopalik <emerycgo@gmail.com>
2016-01-05 17:40:31 -0500

    htmltopdf

--- htmlToPdf.js
+++ htmlToPdf.js
@@ -1,14 +1,14 @@
 var phantom = require('phantom');
-var jsreport = require('jsreport');
+var html = 'file:///Users/emeryotopalik/pdf-module/index.html';
+//var html = 'https://github.com/marcbachmann/node-html-pdf/blob/master/src/scripts/pdf_a4_portrait.coffee';
+var file = 'test.pdf';
 
 phantom.create(function(ph){
   ph.createPage(function(page) {
-    page.open("http://phantomjs.org/screen-capture.html", function(status) {
-      page.render('emery.pdf',function(){
-
+    page.open(html, function(status) {
+      page.render(file,function(){
         console.log('Page Rendered');
         ph.exit();
-
       });
     });
   });
@@ -16,12 +16,16 @@ phantom.create(function(ph){
 
 /*var fs = require('fs');
 var pdf = require('html-pdf');
-var html = fs.readFileSync('index.html', 'utf8');
-var options = {  type: 'pdf', orientation: 'portrait', "border": {
-    "top": "2in",            // default is 0, units: mm, cm, in, px
-    "right": "1in",
-    "bottom": "2in",
-    "left": "1.5in"
-  } };*/
+var html = fs.readFileSync('.html', 'utf8');
+//var html = "<h1>HI<\h1>";
+var options = {  type: 'pdf', orientation: 'portrait', format: 'letter' };
 
+pdf.create(html, options).toFile('test.pdf', function(err, res) {
+  if (err) return console.log(err);
+  console.log(res); // { filename: '/app/businesscard.pdf' }
+});*/
 
+/*
+var pdfMake = require('pdfmake');
+var docDefinition = {content: 'THIS IS CONTENT'};
+pdfMake.createPdf(docDefinition).open();*/
